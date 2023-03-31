0ef745dc4f7748cc5832ea479b4e1d495e38b762
Konson Ak <konakech3@gmail.com>
2023-03-31 18:28:32 +0200

    correct tailwind colors name

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       colors:{
         "dark-purple":"#081A51",
         "light-white":"#344783",
-        "primary":"#E85B2A",
+        "orange":"#E85B2A",
       }
     },
   },
